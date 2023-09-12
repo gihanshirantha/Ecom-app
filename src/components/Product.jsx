cc5ad5a84b763c947678c8efda3cd1fa2f3c6d21
Gihan Shirantha <shirantha1999@gmail.com>
2023-09-12 18:17:21 +0530

    Card creating

---
+++ src/components/Product.jsx
@@ -0,0 +1,41 @@
+import React,{useContext} from 'react';
+import {Link} from 'react-router-dom'
+import {FaPlus} from 'react-icons/fa'
+import { AiFillEye } from "react-icons/ai";
+
+const Product = ({products}) => {
+
+  const {id,image,category,title,price}=products;
+  
+  return (
+    <div>
+      <div className='border border-[e4e4e4] h-[300px] mb-4 relative
+       overflow-hidden group transition rounded-md'>
+        <div className='w-full h-full flex justify-center items-center'>
+          <div className='w-[200] mx-auto flex justify-center items-center'>
+            <img src={image} alt="" className='max-h-[160px] group-hover:scale-110 transition duration-300' />
+          </div>
+          <div>
+            <div className='absolute top-6 -right-11 group-hover:right-5 bg-red-500 rounded-md p-2 flex flex-col justify-center items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-500'  >
+              <button>
+                <div className='flex justify-center items-center text-white w-12 h-12'>
+                  <FaPlus className='text-3xl  ' />
+                </div>
+              </button>
+              <Link to={`/products/${id}`} className='flex justify-center items-center text-primary bg-white rounded-md w-12 h-12 '>
+                <AiFillEye className='text-3xl '/>
+              </Link>
+            </div>
+          </div>
+        </div>
+       </div>
+       <div>
+      <div>{category }</div>
+      <h2 className='font-semibold mb-1'>{title}</h2>
+      <div>{price}</div>
+       </div>
+    </div>
+  );
+};
+
+export default Product;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { ProductContext } from "../contexts/ProductContext";
+import Product from '../components/Product'
 
 const Home = () => {
   //get products from product context
@@ -15,12 +16,13 @@ const Home = () => {
 
   return (
   <div>
-    <section>
+    <section className="py-16">
       <div className="container mx-auto">
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px]
+        max-w-sm mx-auto md:max-w-none md:mx-0">
           {
             filterProducts.map((products)=>{
-              return<div>{products.title}</div>
+              return <Product products={products} key={products.id}/>
             })
           }
         </div>
@@ -28,5 +30,4 @@ const Home = () => {
     </section>
   </div>);
 };
-
 export default Home;
