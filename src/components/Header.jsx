881e6aacd510343c9d308873e8eaf373d7871a19
Gihan Shirantha <shirantha1999@gmail.com>
2023-09-12 23:20:15 +0530

    side bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SidebarContext } from '../contexts/SidebarContext';
 
 const Header = () => {
+  const {isOpen,setIsOpne} = useContext(SidebarContext);
   return <div>
     <div>Header</div>
-    <div>
+    <div onClick={()=>setIsOpne(!isOpen)}>
       Open/close side bar
     </div>
   </div>;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,7 +8,7 @@ import { SidebarContext } from '../contexts/SidebarContext';
 
 const Sidebar = () => {
   const {isOpen,handleClose}=useContext(SidebarContext);
-  return (<div className='w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]'>
+return (<div className={`${isOpen? 'right-0':'-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
     Sidebar</div>);
 };
 
