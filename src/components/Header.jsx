02a593b95e577eeefedbb73a83f89550c1a9f709
Gihan Shirantha <shirantha1999@gmail.com>
2023-09-13 15:41:25 +0530

    Side bar Button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
 import { SidebarContext } from '../contexts/SidebarContext';
+import {BsBag} from 'react-icons/bs'
 
 const Header = () => {
   const {isOpen,setIsOpen} = useContext(SidebarContext);
   return <div>
     <div>Header</div>
-    <button onClick={()=>setIsOpen(!isOpen)}>
-      Open/close side bar
-    </button>
+    <div onClick={()=>setIsOpen(!isOpen)} className='cursor-pointer flrx relative'>
+      <BsBag className='text-2xl'/>
+    </div>
   </div>;
 };
 
