18e21e9dca3160106d2ccda8dba0aae18ba3cb1a
Gihan Shirantha <shirantha1999@gmail.com>
2023-09-17 15:06:44 +0530

    Side bar buttons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -44,7 +44,7 @@ const Sidebar = () => {
           </div>
         </div>
         <Link className="bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium">View Cart</Link>
-        <Link className="bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium">Checkout</Link>
+        <Link className="bg-gray-800 flex p-4 justify-center items-center text-white w-full font-medium">Checkout</Link>
 
       </div>
     </div>
