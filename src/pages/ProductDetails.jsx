f852460a9fe1cb20afd485fe5e55787b16a778d2
Gihan Shirantha <shirantha1999@gmail.com>
2023-09-18 22:35:56 +0530

    Product details page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import  {useParams} from 'react-router-dom'
+import { CartContext } from '../contexts/CartContext';
+import {ProductContext} from '../contexts/ProductContext'
+
 
 const ProductDetails = () => {
+
+    const {id}=useParams();
+    const{products} = useContext(ProductContext);
+    const {addToCart}= useContext(CartContext);
+
+     const Products=products.find((item)=>{
+      return item.id===parseInt(id);
+     });
+
+     if(!Products){
+      return <section className='h-screen flex items-center'>Loading...</section>
+     }
+
+
   return <div>Product Details Page</div>;
 };
 
