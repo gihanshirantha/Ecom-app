036385a809f842874a37860091fac00068fdf823
Gihan Shirantha <shirantha1999@gmail.com>
2023-09-19 12:57:37 +0530

    Prodeuct detaols page create

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,7 +11,7 @@ import { CartContext } from "../contexts/CartContext";
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  const { cart, clearCart, total } = useContext(CartContext);
+  const { cart, clearCart, total,itemAmount } = useContext(CartContext);
   return (
     <div
       className={`${
@@ -19,7 +19,7 @@ const Sidebar = () => {
       } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
     >
       <div className="flex items-center justify-between py-6 border-b">
-        <div className="uppercase font-semibold">Shopping Bag(0)</div>
+        <div className="uppercase font-semibold">Shopping Bag({itemAmount})</div>
         <div onClick={handleClose} className="cursor-pointer w-8 h-8 flex">
           <BsForwardFill className="text-2xl" />
         </div>
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,25 +1,44 @@
-import React,{useContext} from 'react';
-import  {useParams} from 'react-router-dom'
-import { CartContext } from '../contexts/CartContext';
-import {ProductContext} from '../contexts/ProductContext'
-
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { CartContext } from "../contexts/CartContext";
+import { ProductContext } from "../contexts/ProductContext";
 
 const ProductDetails = () => {
+  const { id } = useParams();
+  const { products } = useContext(ProductContext);
+  const { addToCart } = useContext(CartContext);
 
-    const {id}=useParams();
-    const{products} = useContext(ProductContext);
-    const {addToCart}= useContext(CartContext);
+  const Products = products.find((item) => {
+    return item.id === parseInt(id);
+  });
 
-     const Products=products.find((item)=>{
-      return item.id===parseInt(id);
-     });
+  if (!Products) {
+    return <section className="h-screen flex items-center">Loading...</section>;
+  }
 
-     if(!Products){
-      return <section className='h-screen flex items-center'>Loading...</section>
-     }
+  const { title, price, description, image } = Products;
 
-
-  return <div>Product Details Page</div>;
+  return (
+    <section className="pt-32 pb-12 lg:py-32 h-screen flex items-center">
+      <div className="container max-auto">
+        <div className="flex flex-col lg:flex-row items-center">
+          <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0">
+            <img src={image} alt="" className="max-w-[200px] lg:max-w-sm" />
+          </div>
+          <div className="flex-1 text-center lg:text-left">
+            <h1 className="text-[26px] font-medium mb-2 max-w-[450px] max-auto lg:mx-0">
+              {title}
+            </h1>
+            <div className="text-xl text-[#00ADB5] font-medium mb-6">
+              $ {price}
+            </div>
+            <p className="mb-8"> {description}</p>
+            <button onClick={()=>addToCart(Products,Products.id)} className="bg-primary py-4 px-8 text-white">Add To Cart</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProductDetails;
